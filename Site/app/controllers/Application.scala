e47e43f2b54ce479c4ae7752a13f8a001ab3d12c
Maarten Terpstra <s2028980@student.rug.nl>
2014-09-17 10:49:56 +0200

    Added profile page

--- Site/app/controllers/Application.scala
+++ Site/app/controllers/Application.scala
@@ -9,4 +9,8 @@ object Application extends Controller {
     Ok(views.html.index("Sensalizer", "Maarten"))
   }
 
+
+  def profile(title: String, user: String) = Action {
+    Ok(views.html.profile(title, user))
+  }
 }
