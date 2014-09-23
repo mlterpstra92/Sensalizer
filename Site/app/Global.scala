755e1de505b07ed72985a54c613dd71f6ba7068d
folkert <flokkiej@gmail.com>
2014-09-23 14:41:32 +0200

    Added simple errorpage handling (404 and 500)

---
+++ Site/app/Global.scala
@@ -0,0 +1,23 @@
+/**
+ * Created by folkert on 23-9-14.
+ */
+import play.api._
+import play.api.mvc._
+import play.api.mvc.Results._
+import scala.concurrent.Future
+
+
+object Global extends GlobalSettings {
+
+
+  // 500 - internal server error
+  override def onError(request: RequestHeader, throwable: Throwable) = {
+    Future.successful(InternalServerError(views.html.errors.onError(throwable)))
+  }
+
+  // 404 - page not found error
+  override def onHandlerNotFound(request: RequestHeader) = {
+    Future.successful(NotFound(views.html.errors.onHandlerNotFound(request)))
+  }
+
+}
