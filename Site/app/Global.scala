883d1549ea417234b7d21c4a6ef9cf3eb2f5683e
folkert <flokkiej@gmail.com>
2014-09-23 15:30:29 +0200

    Fixed simple errorpage handling (404 and 500)

--- Site/app/Global.scala
+++ Site/app/Global.scala
@@ -9,6 +9,10 @@ import scala.concurrent.Future
 
 object Global extends GlobalSettings {
 
+  // called when a route is found, but it was not possible to bind the request parameters
+  override def onBadRequest(request: RequestHeader, error: String) = {
+    Future.successful(BadRequest("Bad Request: " + error))
+  }
 
   // 500 - internal server error
   override def onError(request: RequestHeader, throwable: Throwable) = {
